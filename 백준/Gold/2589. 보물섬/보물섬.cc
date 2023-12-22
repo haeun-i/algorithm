4f7f92ff06050600e60ffd879f29dfee1c7d057d
Haeun Kim <gkdms1120s@gmail.com>
2023-12-23 01:13:25 +0900

    [Gold V] Title: 보물섬, Time: 124 ms, Memory: 2044 KB -BaekjoonHub

---
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+int graph[50][50];
+int depth[50][50] = { 0, };
+pair<int, int> start = { 0,0 }, end = { 0,0 };
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+int max_n = 0;
+
+
+void bfs(int y, int x) {
+	memset(depth, 0, sizeof(depth));
+	depth[y][x] = 1;
+	queue<pair<int, int>> q;
+	q.push({ y, x });
+
+	int last_y = 0, last_x = 0, last_depth = 0;
+	while (!q.empty()) {
+		pair<int, int> front = q.front();
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int nx = front.second + dx[i];
+			int ny = front.first + dy[i];
+
+			if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+			if (graph[ny][nx] == 1 && !depth[ny][nx]) {
+				depth[ny][nx] = depth[front.first][front.second] + 1;
+				q.push({ ny, nx });
+			}
+		}
+	}
+
+	int max_depth = *max_element(&depth[0][0], &depth[50][50]);
+	max_n = max(max_depth - 1, max_n);
+}
+
+
+int main() {
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+
+		for (int j = 0; j < m; j++) {
+			if (s[j] == 'W') graph[i][j] = 0;
+			else graph[i][j] = 1;
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (graph[i][j] == 1) bfs(i, j);
+		}
+	}
+
+	cout << max_n;
+}
