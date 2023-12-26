0533c33bbe34122f842b64da6f5e3eaef4e8b55d
Haeun Kim <gkdms1120s@gmail.com>
2023-12-26 23:33:00 +0900

    [Silver I] Title: RGB거리, Time: 92 ms, Memory: 12112 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,41 +1,35 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N;
-	static int[][] color;
-	static int[][] arr;
-	
-	public static void main(String[] args) throws Exception {
-		
+	 public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		color = new int[N][3];
-		arr = new int[N][3];
+
+		
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[1001][3];
+		int[][] dp = new int[1001][3];
 		
 		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for(int j=0; j<3; j++) {
-				color[i][j] = Integer.parseInt(st.nextToken());
-			}
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i+1][0] = Integer.parseInt(st.nextToken()); 
+			arr[i+1][1] = Integer.parseInt(st.nextToken()); 
+			arr[i+1][2] = Integer.parseInt(st.nextToken()); 
 		}
 		
+		// 초기화
 		for(int i=0; i<3; i++) {
-			arr[0][i] = color[0][i];
-		}
+			dp[1][i] = arr[1][i];
+		} 
+		 
+		for(int i=2; i<=N; i++) {
+			dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + arr[i][0];
+			dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + arr[i][1];
+			dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + arr[i][2];
+		}	
 		
-		for(int i=1; i<N; i++) {
-			for(int j=0; j<3; j++) {
-				if(j==0) arr[i][j] = Math.min(arr[i-1][1], arr[i-1][2]) + color[i][j];
-				else if(j==1) arr[i][j] = Math.min(arr[i-1][0], arr[i-1][2]) + color[i][j];
-				else if(j==2) arr[i][j] = Math.min(arr[i-1][1], arr[i-1][0]) + color[i][j];
-			}
-		}
-		
-		System.out.println(Math.min(Math.min(arr[N-1][0], arr[N-1][1]), arr[N-1][2]));
-		
-		
-	}
+		int res = Math.min(Math.min(dp[N][0], dp[N][1]), dp[N][2]);
+		System.out.println(res);
+	 }
 }
