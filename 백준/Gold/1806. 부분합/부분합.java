304577ff62e371b8ee61c9e7e240b6fc17b5f735
Haeun Kim <gkdms1120s@gmail.com>
2024-01-14 00:21:49 +0900

    [Gold IV] Title: 부분합, Time: 224 ms, Memory: 23012 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,47 +1,39 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    public static int N, S;
-    public static long[] arr;
-    public static int length = Integer.MAX_VALUE;
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        N = Integer.parseInt(st.nextToken());
-        S = Integer.parseInt(st.nextToken());
-        arr = new long[N];
-
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        cal();
-        if (length == Integer.MAX_VALUE) System.out.println(0);
-        else System.out.println(length);
-    }
-
-    private static void cal() {
-        int l = 0;
-        int r = 0;
-        long sum = 0;
-
-        while (l < N && r < N) {
-            // 확인
-            if (sum + arr[r] >= S) {
-                length = Math.min(r - l + 1, length);
-                sum -= arr[l];
-                l++;
-            } else {
-                sum += arr[r];
-                r++;
-            }
-        }
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N+2];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int l = 0;
+		int r = 0;
+		int res = Integer.MAX_VALUE;
+		
+		long sum = arr[0];
+		while(r <= N) {
+			if(sum < K) {
+				r++;
+				sum += arr[r];
+			}else {
+				res = Math.min(res, r-l+1);
+				sum -= arr[l];
+				l++;
+				
+			}
+		}
+		
+		if (res == Integer.MAX_VALUE) System.out.println(0);
+        else System.out.println(res);
+	}
 }
