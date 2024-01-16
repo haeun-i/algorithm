2a8947a13b0ff04edeb311a7afc7e6002472c37f
Haeun Kim <gkdms1120s@gmail.com>
2024-01-16 23:26:11 +0900

    [Gold IV] Title: 문자열 폭발, Time: 48 ms, Memory: 8756 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string str, bomb;
+	cin >> str >> bomb;
+
+	stack<char> s;
+	int start_idx = 0;
+	int end_idx = 0;
+	for (int i = 0; i < str.size(); i++) {
+		s.push(str[i]);
+		if (s.size() >= bomb.size() && str[i] == bomb.back()) {
+			string tmp = "";
+			for (int i = 0; i < bomb.size(); i++) {
+				tmp += s.top();
+				s.pop();
+			}
+			reverse(tmp.begin(), tmp.end());
+			
+			if (tmp != bomb) {
+				for (int i = 0; i < bomb.size(); i++) {
+					s.push(tmp[i]);
+				}
+			}
+		}
+	}
+	
+	if (s.empty()) { cout << "FRULA"; }
+	else {
+		string tmp = "";
+		while (!s.empty()) {
+			tmp += s.top();
+			s.pop();
+		}
+		reverse(tmp.begin(), tmp.end());
+		cout << tmp;
+	}
+
+
+}
