308af39d2b1e22b58565ad70e08aa1cb2eb5926c
Haeun Kim <gkdms1120s@gmail.com>
2024-01-29 21:54:12 +0900

    [Gold IV] Title: 부분합, Time: 216 ms, Memory: 22400 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,39 +1,39 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int[] arr = new int[N+2];
+		int M = Integer.parseInt(st.nextToken());
 		
-		st = new StringTokenizer(br.readLine(), " ");
+		int[] arr = new int[N+2];
+		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		int l = 0;
 		int r = 0;
+		
+		int sum = arr[0];
 		int res = Integer.MAX_VALUE;
 		
-		long sum = arr[0];
 		while(r <= N) {
-			if(sum < K) {
+			if(sum < M) {
 				r++;
 				sum += arr[r];
 			}else {
 				res = Math.min(res, r-l+1);
 				sum -= arr[l];
 				l++;
-				
+			
 			}
 		}
 		
-		if (res == Integer.MAX_VALUE) System.out.println(0);
-        else System.out.println(res);
+		if(res == Integer.MAX_VALUE) System.out.println(0);
+		else System.out.println(res);
 	}
 }
