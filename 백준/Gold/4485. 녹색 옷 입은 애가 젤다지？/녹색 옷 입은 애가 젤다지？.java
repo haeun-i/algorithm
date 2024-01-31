5ba79ef9c0b6e49f7f83fb6c6d088ecbf7a4b87a
Haeun Kim <gkdms1120s@gmail.com>
2024-01-31 09:33:10 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 184 ms, Memory: 19364 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -3,76 +3,79 @@ import java.io.*;
 
 public class Main {
 	
-	public static int[] dy = {0, 1, 0, -1};
-	public static int[] dx = {1, 0, -1, 0};
-
+	static int[][] cave;
+	static int[][] dist;
+	static PriorityQueue<Node> pq;
+	
+	static int[] dy = {0, 1, 0, -1};
+	static int[] dx = {1, 0, -1, 0};
+	
 	public static class Node implements Comparable<Node>{
-		int r;
-		int c;
+		int[] dest;
 		int dist;
 		
-		public Node(int r, int c, int dist) {
-			super();
-			this.r = r;
-			this.c = c;
+		Node(int[] dest, int dist){
+			// 위치의 x, y
+			this.dest = new int[2];
+			this.dest[0] = dest[0];
+			this.dest[1] = dest[1];
+			
+			// 가중치
 			this.dist = dist;
 		}
 		
 		@Override
-		public int compareTo(Node o) {
-			return this.dist - o.dist;
+		public int compareTo(Node n) {
+			return this.dist - n.dist;
 		}
 	}
 	
-	public static int[][] arr;
-	public static int[][] dist;
-	public static PriorityQueue<Node> queue;
-	
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		int tc = 0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
 		int N = Integer.parseInt(br.readLine());
+		int tc = 0;
 		while(N != 0) {
 			tc++;
-			queue = new PriorityQueue<>();
-			arr = new int[N][N];
+			cave = new int[N][N];
 			dist = new int[N][N];
 			
+			StringTokenizer st;
 			for(int i=0; i<N; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
 				for(int j=0; j<N; j++) {
+					cave[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) { // 최댓값으로 초기화
 					dist[i][j] = Integer.MAX_VALUE;
 				}
 			}
 			
-			for(int i=0; i<N; i++) {
-				st = new StringTokenizer(br.readLine(), " ");
-				for(int j=0; j<N; j++) {
-					arr[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
+			pq = new PriorityQueue<>();
+			pq.add(new Node(new int[] {0, 0}, cave[0][0])); // 출발지
+			dist[0][0] = cave[0][0];		
 			
-			queue.add(new Node(0, 0, arr[0][0]));
-			dist[0][0] = arr[0][0];
-			while(!queue.isEmpty()) {
-				Node tmp = queue.poll();
-				
+			while(!pq.isEmpty()) {
+				Node node = pq.poll();
 				for(int i=0; i<4; i++) {
-					int ny = tmp.r + dy[i];
-					int nx = tmp.c + dx[i];
+					// 다음 위치
+					int ny = node.dest[0] + dy[i];
+					int nx = node.dest[1] + dx[i];
 					
+					// 넘어가면 불가
 					if(ny < 0 || ny >= N || nx < 0 || nx >= N) continue;
-					if(dist[ny][nx] > dist[tmp.r][tmp.c] + arr[ny][nx]) {
-						dist[ny][nx] = dist[tmp.r][tmp.c] + arr[ny][nx];
-						queue.add(new Node(ny, nx, dist[ny][nx]));
+					
+					if(dist[ny][nx] > node.dist + cave[ny][nx]) {
+						dist[ny][nx] = node.dist + cave[ny][nx];
+						pq.add(new Node(new int[] {ny, nx}, dist[ny][nx]));
 					}
 				}
 			}
-			
-			System.out.println("Problem " + tc +": " + dist[N-1][N-1]);
-			N = Integer.parseInt(br.readLine());
-				
+			System.out.println("Problem " + tc + ": " + dist[N-1][N-1]);
+			N = Integer.parseInt(br.readLine()); // N 갱신
 		}
 	}
+
 }
