98653a5fa2bd3c2abb09f31852b3d2fb6cc1f803
Haeun Kim <gkdms1120s@gmail.com>
2024-04-20 21:02:11 +0900

    [level 1] Title: 시저 암호, Time: 4.13 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -1,11 +1,6 @@
 def solution(s, n):
     answer = ''
     for c in s:
-        if(ord(c) >= 65 and ord(c) <= 90):
-            answer += chr(((ord(c)-65+n)%26)+65)
-        elif(ord(c) >= 97 and ord(c) <= 122):
-            print(ord(c))
-            answer += chr(((ord(c)-97+n)%26)+97)
-        else:
-            answer += c
+        print(chr(c+n))
+    
     return answer
