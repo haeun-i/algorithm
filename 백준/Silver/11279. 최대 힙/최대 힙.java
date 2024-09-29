dbe2e269b6301247c9ac079db4d88fc1f5a950cf
Haeun Kim <gkdms1120s@gmail.com>
2024-09-29 16:06:13 +0900

    [Silver II] Title: 최대 힙, Time: 1024 ms, Memory: 32624 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	static int N;
+	static PriorityQueue<Integer> pq;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		pq = new PriorityQueue<>(Collections.reverseOrder());
+		
+		for(int i=0; i<N; i++) {
+			int x = Integer.parseInt(br.readLine());
+			if(x == 0) {
+				if(pq.size() == 0) System.out.println(0);
+				else {
+					System.out.println(pq.poll());
+				}
+			}else {
+				pq.offer(x);
+			}
+		}
+	}
+}
