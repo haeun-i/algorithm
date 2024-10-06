2e9994268d1a8f8a126e31905f863a79410bb53c
Haeun Kim <gkdms1120s@gmail.com>
2024-10-06 23:56:34 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 228 ms, Memory: 74196 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+ public class Main {
+     
+    public static StringBuilder sb = new StringBuilder();
+    public static int N;
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        
+        sb.append((int)Math.pow(2,N)-1).append('\n');
+        hanoi(1, 2, 3, N);
+        System.out.println(sb);
+    }
+     
+    static void hanoi(int from, int m, int to, int N){
+        if(N == 0) return;
+        
+        hanoi(from, to, m, N-1);
+        sb.append(from+" "+to+'\n');
+        hanoi(m,from,to,N-1);
+    }
+ }
