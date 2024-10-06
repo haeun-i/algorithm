10225faccf1155fe35d4cb401d3b48ab0b0d7b0b
Haeun Kim <gkdms1120s@gmail.com>
2024-10-06 20:46:18 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 96 ms, Memory: 14968 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+    
+    public static int max=Integer.MIN_VALUE;
+    public static int min=Integer.MAX_VALUE;
+    public static int[] number;
+    public static int[] operator =new int[4];
+    public static int N;
+    
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        number = new int[N];
+        
+        for(int i=0; i<N; i++){
+            number[i]=sc.nextInt();
+        }
+        for (int i=0;i<4;i++){
+            operator[i]=sc.nextInt();
+        }
+        
+        dfs(number[0], 1);
+        System.out.println(max);
+        System.out.println(min);
+    }
+    
+    public static void dfs(int num,int idx){
+        
+        if(idx==N){
+            max= Math.max(max,num);
+            min= Math.min(min,num);
+            return;
+        }
+        
+        for(int i=0;i<4;i++){
+            if (operator[i]>0){
+                operator[i]--;
+                switch (i){
+                    case 0: dfs(num+number[idx],idx+1); break;
+                    case 1: dfs(num-number[idx],idx+1); break;
+                    case 2: dfs(num*number[idx],idx+1); break;
+                    case 3: dfs(num/number[idx],idx+1); break;
+                }
+                operator [i]++;
+            }
+        }
+        
+    }
+}
